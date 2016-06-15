8c34a0161221cc36439d06999471b6a03c95bc46
= <=>
2016-06-15 16:44:20 +0100

    extract scaffolding for avl in future

---
+++ datastructures/bst/avl.go
@@ -0,0 +1,17 @@
+package bst
+
+import "github.com/parallelKiller/fundamentals/datastructures/tree"
+
+type avlTree struct {
+	binarySTree
+}
+
+func (b *avlTree) Add(node tree.Node) {}
+
+func (b *avlTree) Remove(node tree.Node) {}
+
+func (b *avlTree) RotateLeft() {
+}
+
+func (b *avlTree) RotateRight() {
+}
---
+++ datastructures/bst/avl_test.go
@@ -0,0 +1,11 @@
+package bst_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Avl", func() {
+
+	It("is a self balancing binary search tree", func() {
+	})
+})
