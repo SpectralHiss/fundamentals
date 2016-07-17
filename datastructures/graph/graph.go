ef15f0a3ef1635dc23812ef1460be5ab0dc9bc00
Houssem <hassoum92@hotmail.com>
2016-07-17 16:47:19 +0100

    WIP graph

---
+++ datastructures/graph/graph.go
@@ -0,0 +1,41 @@
+package graph
+
+// let's start with simple DAG
+
+type Edge struct {
+	EndNode  int // number of vertex
+	Distance int
+}
+
+type DWGraph struct {
+	matrix [][]int
+}
+
+type Graph interface {
+	AdjList(node int) []Edge
+	Matrix() [][]int
+}
+
+func New(InitialMatrix [][]int) Graph {
+	return DWGraph{
+		matrix: InitialMatrix,
+	}
+}
+
+func (graph DWGraph) AddEdge(node int, edge Edge) {
+	graph.matrix[node][edge.EndNode] = edge.Distance
+}
+
+func (graph DWGraph) Matrix() [][]int {
+	return graph.matrix
+}
+
+func (graph DWGraph) AdjList(node int) []Edge {
+	var adj []Edge
+	for node, distance := range graph.matrix[node] {
+		if distance != 0 {
+			adj = append(adj, Edge{EndNode: elem, Distance: distance})
+		}
+	}
+	return adj
+}
---
+++ datastructures/graph/graph_test.go
@@ -0,0 +1,12 @@
+package graph_test
+
+import (
+	. "github.com/SpectralHiss/fundamentals/datastructures/graph"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Graph", func() {
+
+})
